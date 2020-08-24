9985c10e1de0f32841476608a6d15803175fe7c7
gaiyadev <gaiyaobed94@gmail.com>
2020-08-25 00:51:34 +0100

    Add: Geting all product using provider

---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/providers/product_provider.dart';
+import 'package:state_management/widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    final product = productData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+      ),
+      itemBuilder: (ctx, i) => ProductItem(
+        product[i].id,
+        product[i].title,
+        product[i].imageUrl,
+      ),
+      itemCount: product.length,
+    );
+  }
+}
