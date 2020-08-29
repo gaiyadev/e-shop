31982e60682b58ee38f77ae2ecadd8b06f06a616
gaiyadev <gaiyaobed94@gmail.com>
2020-08-29 19:15:41 +0100

    Feat: expandable to view product on order screeen

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../providers/orders_provider.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +22,55 @@ class OrderItem extends StatelessWidget {
         children: [
           ListTile(
             title: Text(
-              'NGN ${order.amount}',
+              'NGN ${widget.order.amount}',
+              style: TextStyle(
+                color: Colors.purple,
+                fontSize: 23.0,
+              ),
             ),
             // subtitle: Text(
             //   DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
             // ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
+              height: min(widget.order.products.length * 20.0 + 10.0, 100.0),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            '${prod.quantity}x NGN ${prod.price}',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
