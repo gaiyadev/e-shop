32326b0ef4bacf83fcecabd13e0bfa8755b7ca94
gaiyadev <gaiyaobed94@gmail.com>
2020-09-01 09:00:58 +0100

    Add: added CircularProgressIndication

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:state_management/providers/cart.dart';
+import 'package:http/http.dart' as http;
 
 class OrderItem {
   final String id;
@@ -22,14 +25,33 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProduct, double total) {
+  Future<void> addOrder(List<CartItem> cartProduct, double total) async {
+    const url = 'https://e-store-3adcd.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+
+    final response = await http.post(url,
+        body: jsonEncode({
+          'amount': total,
+          'products': cartProduct
+              .map((e) => {
+                    'id': e.id,
+                    'title': e.title,
+                    'quantity': e.quantity,
+                    'price': e.price,
+                  })
+              .toList(),
+          'dateTime': timestamp.toIso8601String(),
+        }));
+    if (response.statusCode >= 400) {
+      print(response.statusCode);
+    }
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: jsonDecode(response.body)['name'],
         amount: total,
         products: cartProduct,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
       ),
     );
     notifyListeners();
